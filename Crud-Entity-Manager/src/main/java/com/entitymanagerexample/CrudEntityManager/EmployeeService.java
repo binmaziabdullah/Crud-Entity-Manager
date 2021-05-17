df905c8277ff346f4048db998e7b044e043f1e65
binmaziabdullah <80192810+binmaziabdullah@users.noreply.github.com>
2021-05-17 04:27:41 -0800

    Update EmployeeService.java

--- Crud-Entity-Manager/src/main/java/com/entitymanagerexample/CrudEntityManager/EmployeeService.java
+++ Crud-Entity-Manager/src/main/java/com/entitymanagerexample/CrudEntityManager/EmployeeService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
 
